00317645828c3658dcbd650e2db62cf16f44c73c
Marius Guceanu <Marius Guceanu@HZ114828.H012108.NET>
2023-03-07 08:31:27 +0100

    objetos y clases

--- PolideportivoJS/src/modelo/Inscripciones.java
+++ PolideportivoJS/src/modelo/Inscripciones.java
@@ -2,4 +2,43 @@ package modelo;
 
 public class Inscripciones {
 
+	private int id;
+	private String id_actividad;
+	private String id_usuario;
+
+	public String toString() {
+		return "Inscripciones [id=" + id + ", id_actividad=" + id_actividad + ", id_usuario=" + id_usuario + "]";
+	}
+
+	public Inscripciones(int id, String id_actividad, String id_usuario) {
+		super();
+		this.id = id;
+		this.id_actividad = id_actividad;
+		this.id_usuario = id_usuario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getId_actividad() {
+		return id_actividad;
+	}
+
+	public void setId_actividad(String id_actividad) {
+		this.id_actividad = id_actividad;
+	}
+
+	public String getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(String id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
 }
